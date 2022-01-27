19274b3af9f3815a48cefcc918ff05a193ec1a7b
Mohit Rawat <mohitrawat48@gmail.com>
2022-01-27 15:00:43 -0500

    h2 database

--- src/main/java/com/demo/gnotesapp/GNotesappApplication.java
+++ src/main/java/com/demo/gnotesapp/GNotesappApplication.java
@@ -1,9 +1,14 @@
 package com.demo.gnotesapp;
 
+import com.demo.gnotesapp.entity.Note;
+import com.demo.gnotesapp.service.intrface.NotesService;
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class GNotesappApplication extends SpringBootServletInitializer {
@@ -15,7 +20,12 @@ public class GNotesappApplication extends SpringBootServletInitializer {
 	}
 
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(GNotesappApplication.class, args);
-	}
+		ConfigurableApplicationContext context = SpringApplication.run(GNotesappApplication.class, args);
 
+		Note note = new Note();
+		note.setNotecontent("My First Note");
+
+		NotesService notesService = context.getBean(NotesService.class);
+		notesService.addNote(note);
+	}
 }
